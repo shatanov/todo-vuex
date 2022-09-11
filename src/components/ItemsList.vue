a1d697bc7dabfaae77c843238b90e47cea3b79a8
shatanov <timur-shatanov.work@mail.ru>
2022-09-11 20:42:40 +0300

    add ItemList component

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,25 @@
+<script>
+import { computed } from "vue";
+import TodoItem from "./TodoItem.vue";
+import { useStore } from "vuex";
+
+export default {
+    components: {
+        TodoItem,
+    },
+    setup() {
+        const store = useStore();
+        const todos = computed(() => store.state.todos);
+
+        return { todos };
+    },
+};
+</script>
+
+<template>
+    <div v-for="todo in todos"
+    :key="todo.text"
+    >
+        <todo-item :todo="todo"/>
+    </div>
+</template>
