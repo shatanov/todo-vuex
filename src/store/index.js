f366c81efbaa441f97c248e985c48957f6a73105
shatanov <timur-shatanov.work@mail.ru>
2022-09-11 20:41:21 +0300

    add vuex state, mutations and actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,33 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
-
+    state: {
+        todos: [
+        ],
+    },
+    mutations: {
+        addTodo(state, todo) {
+            state.todos.push(todo);
+        },
+        deleteTodo(state, todo) {
+            const todoIdx = state.todos.indexOf(todo);
+            state.todos.splice(todoIdx, 1);
+        },
+        changeTodoStatus(state, payload) {
+            const todoIdx = state.todos.indexOf(payload.todo);
+            const todo = state.todos[todoIdx];
+            todo.active = payload.active;
+        },
+    },
+    actions: {
+        addTodo({ commit }, todo) {
+            commit('addTodo', todo);
+        },
+        removeTodo({ commit }, todo) {
+            commit('deleteTodo', todo);
+        },
+        changeTodoStatus({ commit }, {todo, active}) {
+            commit('changeTodoStatus', {todo, active});
+        },
+    },
 });
